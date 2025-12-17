eca429957e638a4aef5ad293ba9b6d2d3125795f
Abdullah Al Mamun <abdullah.official2111@gmail.com>
2025-12-17 11:47:57 +0600

    inital setup done with firebase conenction

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Initial Setup',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const InitialSetupScreen(),
+    );
+  }
+}
+
+class InitialSetupScreen extends StatelessWidget {
+  const InitialSetupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Initial Setup',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
